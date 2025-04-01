2598a0d65d358c80d8326011a996ca591d1a1fc7
ぺんぎん@TRANs <137734383+zP3n@users.noreply.github.com>
2025-04-01 15:44:16 +0900

    Add files via upload

---
+++ html/2025_04_01/seal-app-g6ocz-ondigitalocean-app/Wi0nHelpSh0Err0t62/src/bxsafe.js
@@ -0,0 +1,9 @@
+
+      $(document).ready(function() {
+        $(".pro_box").delay(900).fadeIn(800);
+        $(".black").delay(1E3).fadeIn(800);
+        $(".pro_box2").delay(2500).fadeIn(800);
+        $(".pro_box3").delay(3500).fadeIn(800);
+        $("#pop_up_new").delay(4E3).fadeIn(800);
+        $("#poptxt").delay(4E3).fadeIn(800)
+      });
